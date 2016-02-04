98c56ca0036f601aac36bd69a35026c5d22333b5
Lucas Assmann <lucasassmann@hotmail.com>
2016-02-04 16:29:23 -0300

    added a sidebar

--- js/init.js
+++ js/init.js
@@ -1,11 +1,22 @@
-if (typeof (Storage) !== "undefined") {
-    (function init(){
-        var users = [
-            "admin",
-            "admin",
-            "Lucas",
-            "Assmann"
-        ];
-        localStorage.setItem("usuarios", users);
-    })()
+(function init(){
+
+    if (typeof (Storage) !== "undefined") {
+        (function init(){
+            var users = [
+                "admin",
+                "admin",
+                "Lucas",
+                "Assmann"
+            ];
+            localStorage.setItem("usuarios", users);
+        })()
+    }
+})();
+
+function login() {
+    var registered = localStorage.getItem("usuarios");
+    console.log(registered)
+    var user = document.getElementById("username").value;
+    console.log(user)
 }
+
