8eb6c70db5d0eaa2d068801e10a0242f79b20f0d
Lucas Assmann <lucasassmann@hotmail.com>
2016-02-05 16:42:48 -0300

    asdsa

--- js/init.js
+++ js/init.js
@@ -1,11 +1,14 @@
 var users = [];
 var userLogged;
 (function init() {
-    var user1 = new User('admin', 'admin');
-    var user2 = new User('lucas', 'lucas');
-    users[user1.getUsername()] = user1;
-    users[user2.getUsername()] = user2;
-    storeUsers();
+    if(!localStorage.getItem('users')){
+        console.log('true')
+        var user1 = new User('admin', 'admin');
+        var user2 = new User('lucas', 'lucas');
+        users[user1.getUsername()] = user1;
+        users[user2.getUsername()] = user2;
+        storeUsers();
+    }
 })();
 
 function storeUsers() {
@@ -22,19 +25,26 @@ function storeUsers() {
 function validateLogin() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("passLogin").value;
-    if(users[username] && users[username].getPassword() === password){
-        console.log("isisisi")
-        sessionStorage.setItem("name", username)
+    if (users[username] && users[username].getPassword() === password) {
+        console.log("entramos")
+        sessionStorage.setItem("name", username);
         $("#login").modal('hide');
-    }else{
-
-        console.log("pass incorrecta")
+    } else {
         $('#passform').after("<div class='alert alert-danger'>Incorrect password. Try again o register an account.</div>")
-
     }
 
 }
 
 function registerUser() {
-    console.log("hola")
+    var username = document.getElementById("userReg").value;
+    var pass = document.getElementById("passReg").value;
+    if(username.length === 0){
+        $('#passRegister').after("<div class='alert alert-danger'>You have to introduce a username</div>")
+    }else {
+        var user = new User(username, pass);
+        users[user.getUsername()] = user;
+        storeUsers();
+        $("#register").modal('hide')
+    }
+
 }
